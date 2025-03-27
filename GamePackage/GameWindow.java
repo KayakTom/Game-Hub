20dff97e1225c2b4254e75e6f91c0501434eb7ff
KayakTom <tom159cnc65@outlook.com>
2025-03-27 13:19:36 +0000

    Game Package files on correct branch

---
+++ GamePackage/GameWindow.java
@@ -0,0 +1,12 @@
+package GamePackage;
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    private String title;
+    private int height;
+    public int width;
+
+    public GameWindow(int width, int height, boolean createWindow){
+        
+    }
+}
