9d3e4b8b550554d387008d4f40e34c9f977a617b
KayakTom <tom159cnc65@outlook.com>
2025-07-22 17:04:06 +0100

    Created the Driver class that will be the primary caller

---
+++ src/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    
+    public static void main(String[] args) {
+        // Create an instance of the Menu class to display the game hub
+        Menu gameMenu = new Menu();
+    }
+}
