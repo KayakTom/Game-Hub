73058d0ef56048c3a476d8f3b3caabd580839302
KayakTom <tom159cnc65@outlook.com>
2025-07-23 18:47:09 +0100

    Made it possible to load a gameMenu instance

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,10 @@
+import Main_Menu.Menu;
+
 public class Driver {
     
     public static void main(String[] args) {
         // Create an instance of the Menu class to display the game hub
         Menu gameMenu = new Menu();
+        gameMenu.loadMenu();
     }
 }
