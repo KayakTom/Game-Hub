64b129e4dccf9b959785ecc3b4004238e157cf26
KayakTom <tom159cnc65@outlook.com>
2025-07-25 18:23:03 +0100

    Made it so that the main menu is generated through the constructor, with the image displayed

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,5 @@ public class Driver {
     public static void main(String[] args) {
         // Create an instance of the Menu class to display the game hub
         Menu gameMenu = new Menu();
-        gameMenu.loadMenu();
     }
 }
--- src/Main_Menu/Menu.java
+++ src/Main_Menu/Menu.java
@@ -3,36 +3,30 @@ package Main_Menu;
 import java.awt.*;
 import javax.swing.*;
 
-import Libraries.GameArena.GameArena;
+import Libraries.JPanelBackgroundImage.BackgroundImage;
 
 public class Menu {
     // Gets the user screen dimensions (height and width)
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    private int windowWidth = (int)screenSize.getWidth();
-    private int windowHeight = (int)screenSize.getHeight();
+    private int windowWidth = (int)(screenSize.getWidth() / 1.5);
+    private int windowHeight = (int)(screenSize.getHeight() / 1.5);
 
     private JFrame Window;
-    private JPanel menu;
-    private SpringLayout layout;
-
-    private GameArena mainMenu = new GameArena(windowWidth, windowHeight, false);
-
+    private SpringLayout layout = new SpringLayout();
+    private BackgroundImage menu = new BackgroundImage("/Images/Game Hub Background.jpg");
 
     public Menu(){
         // Initialises the game hub menu
         this.Window = new JFrame("Game Hub");
-        this.menu = new JPanel();
         this.menu.setLayout(layout);
 
         this.Window.setSize(this.windowWidth, this.windowHeight);
+        this.menu.setPreferredSize(new Dimension(this.windowWidth, this.windowHeight));
         this.Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.Window.setResizable(false); // Temporary until we get everything adaptable to the screen size
-    }
+        this.Window.setLocationRelativeTo(null); // Centers the window on the screen
 
-    public void loadMenu(){
-        this.mainMenu.setBackgroundImage("Images/gameHubBackground.png");
-
-        this.Window.add(this.menu);
+        this.Window.setContentPane(this.menu);
+        this.Window.setVisible(true);
     }
 }
