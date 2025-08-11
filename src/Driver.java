a21d9e588f0c744e3da24b0e042a5d3a9be74760
KayakTom <tom159cnc65@outlook.com>
2025-08-11 13:19:26 +0100

    Universalised (added to Driver.java) key Window setup aspects

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,26 @@
 import Main_Menu.Menu;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Driver {
     
     public static void main(String[] args) {
         // Create an instance of the Menu class to display the game hub
-        Menu gameMenu = new Menu();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Menu mainMenu;
+        JFrame Window = new JFrame("Game Hub");
+        int windowWidth = (int)(screenSize.getWidth() / 1.5);
+        int windowHeight = (int)(screenSize.getHeight() / 1.5);
+
+        Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Sets the default close operation
+        Window.setSize(windowWidth, windowHeight); // Sets the size of the window
+        Window.setResizable(false); // Temporary until we get everything adaptable to the screen size
+        Window.setLocationRelativeTo(null); // Centers the window on the screen
+
+        mainMenu = new Menu(Window, windowWidth, windowHeight);
+
+        // ISSUE: Would need to run forever and forever loops not good practice (chaining is solution)
+      
     }
 }
--- src/Main_Menu/Menu.java
+++ src/Main_Menu/Menu.java
@@ -1,32 +1,61 @@
 package Main_Menu;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 import Libraries.JPanelBackgroundImage.BackgroundImage;
 
-public class Menu {
-    // Gets the user screen dimensions (height and width)
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    private int windowWidth = (int)(screenSize.getWidth() / 1.5);
-    private int windowHeight = (int)(screenSize.getHeight() / 1.5);
-
+public class Menu implements ActionListener {
     private JFrame Window;
+    private int width, height;
+
     private SpringLayout layout = new SpringLayout();
     private BackgroundImage menu = new BackgroundImage("/Images/Game Hub.png");
+    private Icon playButtonImage = new ImageIcon(getClass().getResource("/Images/PLAY.png"));
 
-    public Menu(){
+    private JButton playButton = new JButton();
+
+    public Menu(JFrame Window, int width, int height) {
         // Initialises the game hub menu
-        this.Window = new JFrame("Game Hub");
+        this.Window = Window;
+        this.width = width;
+        this.height = height;
+
         this.menu.setLayout(layout);
 
-        this.Window.setSize(this.windowWidth, this.windowHeight);
-        this.menu.setPreferredSize(new Dimension(this.windowWidth, this.windowHeight));
-        this.Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.Window.setResizable(false); // Temporary until we get everything adaptable to the screen size
-        this.Window.setLocationRelativeTo(null); // Centers the window on the screen
+        this.menu.setVisible(false);
+
+        this.menu.setPreferredSize(new Dimension(width, height));
 
         this.Window.setContentPane(this.menu);
         this.Window.setVisible(true);
+        
+        this.layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, this.playButton, 0, SpringLayout.HORIZONTAL_CENTER, this.menu);
+        this.layout.putConstraint(SpringLayout.VERTICAL_CENTER, this.playButton, 0, SpringLayout.VERTICAL_CENTER, this.menu);
+
+        this.playButton.setPreferredSize(new Dimension(300, 75));
+        this.playButton.setIcon(this.playButtonImage);
+        this.playButton.addActionListener(this);
+
+
+        this.menu.add(playButton);
+        this.menu.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // This method will handle the actions for the buttons in the menu
+        // For now, it does nothing
+
+        if(e.getSource() == this.playButton && this.Window.getContentPane() == this.menu){
+            this.playButton.setEnabled(false);
+            this.playButton.setVisible(false);
+            // System.out.println("Play button clicked, transitioning to game selection menu...");
+            // Instantiate the game selection menu here
+            System.out.println("Initializing game selection menu...");
+            GameSelection gameSelection = new GameSelection(this.Window, this.width, this.height);
+        }
+
     }
 }
